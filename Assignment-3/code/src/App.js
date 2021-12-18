c97b89dab558df31138b8249f176f5b58553ac50
k mohammed althaf <kmalthaf79@gmail.com>
2021-12-18 11:30:39 +0530

    final submission

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,13 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-
-var arrow="<"
-function App() {
+function App(props) {
+  const {employee}=props;
   return (
-    <div className="site-container">
-    {/* Start Your code here */}
+    <div className="App">
+      <div className='head'>
+        <div  className='btn1'>
+          <button className='btn11' type='submit' value='<'> {'<'}</button>
+        </div>
+        <div className='name-id'>
+          <h3 style={{margin:'2px'}}><strong>{employee.name}</strong></h3>
+          <p  style={{margin:'2px'}}>{employee.id}</p>
+        </div>
+        <div  className='btn2'>
+          <button className='btn22' type='submit'>Print</button>
+        </div>
+      </div>
+      <div className='customer-info'>
+        <p><strong>Appointment : </strong>{employee.appointment}</p>
+        <p><strong>Email : </strong>{employee.email}</p>
+        <p><strong>Phone : </strong>{employee.phone}</p>
+
+
+      </div>
+      <div className='order-info'>
+      <div  className='status'>
+          <h3>Status</h3>
+          <ul>
+            <li>{employee.status}</li>
+          </ul>
+        </div>
+        <div className='door'>
+          <h3>Door</h3>
+          <p>{employee.door}</p>
+        </div>
+        <div  className='time'>
+        <h3>Time</h3>
+        <p>{employee.time} </p>
+        </div>
+      </div>
+      <div className='product-list'>
+        <div className='check'>
+          <input type='checkbox'/>
+        </div>
+        <div className='profile-pic'>
+            <img src={employee.profileImg} alt='profile' />
+        </div>
+        <div className='description'>
+          <h3>Description</h3>
+          <p>{employee.description} </p>
+
+        </div>
+
+      </div>
     </div>
   );
 }
--- Assignment-3/code/src/index.js
+++ Assignment-3/code/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const employee={
+  name:'Albert',
+  id:'00004352',
+  appointment:'10:00 AM (17-12-20121)',
+  email:'albert@gmail.com',
+  phone:9876543210,
+  status:'In progress',
+  door:'Mark',
+  time:'10:30(18-12-2021)',
+  profileImg: 'https://www.w3schools.com/howto/img_avatar.png',
+  description:'A reliable and dependable employee is a trusted one. Reliability is an important factor in knowing that the job will get done and it will get done well.'
 
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  employee={employee}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
